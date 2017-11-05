bb29efc1be0d1b62f2559df9d654b7db87e41a15
Sébastien Wautelet <sebwautelet@gmail.com>
2017-11-05 21:22:19 +0100

    First commit

---
+++ Assets/Scripts/AugmentedDominionBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using EasyAR;
+using System.Xml;
+
+namespace AugmentedDominion
+{
+  public class AugmentedDominionBehaviour : MonoBehaviour
+  {
+    public GameObject cardPrefab;
+    private ImageTrackerBehaviour imageTrackerBehaviour;
+
+    // Use this for initialization
+    void Start()
+    {
+      imageTrackerBehaviour=GameObject.Find("ImageTracker").GetComponent<ImageTrackerBehaviour>();
+      TextAsset textAsset = (TextAsset)Resources.Load("cards", typeof(TextAsset));
+      XmlDocument xmlCards = new XmlDocument();
+      xmlCards.LoadXml(textAsset.text);
+      XmlNodeList xmlCardsList=xmlCards.SelectNodes("/Cards/Card");
+      foreach (XmlNode xmlCard in xmlCardsList)
+      {
+        InitCard(xmlCard);
+      }
+    }
+
+    private void InitCard(XmlNode xmlCard)
+    {
+      string cardId = xmlCard.Attributes["id"].InnerText;
+
+      Debug.Log("instantiating " + cardId);
+      GameObject card=Instantiate(cardPrefab);      
+      DominionCardTargetBehaviour behaviour=card.GetComponent<DominionCardTargetBehaviour>();
+      behaviour.Path = cardId + ".jpg";
+      behaviour.Name = cardId;
+      behaviour.Bind(imageTrackerBehaviour);
+      behaviour.CardDescription = xmlCard.InnerText;
+    }
+  }
+}
---
+++ Assets/Scripts/DominionCardTargetBehaviour.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using EasyAR;
+
+namespace AugmentedDominion
+{
+  public class DominionCardTargetBehaviour : ImageTargetBehaviour
+  {
+    private Canvas canvasObject;
+    private Text cardNameText;
+    private Text cardDescriptionText;
+    public string CardDescription { get;  set; }
+
+    protected override void Awake()
+    {
+      base.Awake();
+      canvasObject = GameObject.Find("CardInfoCanvas").GetComponent<Canvas>();
+      canvasObject.enabled = false;
+      cardNameText = GameObject.Find("CardNameText").GetComponent<Text>();
+      cardDescriptionText = GameObject.Find("CardDescriptionText").GetComponent<Text>();
+      TargetFound += OnTargetFound;
+      TargetLost += OnTargetLost;
+      TargetLoad += OnTargetLoad;
+      TargetUnload += OnTargetUnload;
+    }
+
+    void OnTargetFound(TargetAbstractBehaviour behaviour)
+    {
+      Debug.Log("Found: " + Target.Name);
+      canvasObject.enabled = true;
+      cardNameText.text = Target.Name;
+      cardDescriptionText.text = CardDescription;
+    }
+
+    void OnTargetLost(TargetAbstractBehaviour behaviour)
+    {
+      Debug.Log("Lost: " + Target.Name);
+      canvasObject.enabled = false;
+    }
+
+    void OnTargetLoad(ImageTargetBaseBehaviour behaviour, ImageTrackerBaseBehaviour tracker, bool status)
+    {
+      Debug.Log("Load target (" + status + "): " + Target.Id + " (" + Target.Name + ") " + " -> " + tracker);
+    }
+
+    void OnTargetUnload(ImageTargetBaseBehaviour behaviour, ImageTrackerBaseBehaviour tracker, bool status)
+    {
+      Debug.Log("Unload target (" + status + "): " + Target.Id + " (" + Target.Name + ") " + " -> " + tracker);
+    }
+  }
+}
